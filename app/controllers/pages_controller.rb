c5444b99bc22c57c57f263e6fb59b32db2c1dbc2
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-07-22 13:52:13 -0700

    Replaced Welcome Controller with Pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def home
+    @message = Message.new
+  end
+
+  def contact
+  end
+
+  def download_resume
+    send_file(
+    "#{Rails.root}/public/resume.pdf",
+    filename: "SebastianValdez_Resume.pdf",
+    type: "application/pdf"
+    )
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'welcome#home'
   get  'contact-me',			to: 'messages#new',			as: 'new_message'
-  get  'resume-download',	to: 'welcome#download_resume',		as: 'download_resume'
   post 'contact-me',			to: 'messages#create',	as: 'create_message'
+  get  'resume-download', to: 'welcome#download_resume',    as: 'download_resume'
+  root 'pages#home'
 end
