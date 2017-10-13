884e925788fe10419ebe7bf65116ac61c356c285
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-10-12 17:28:33 -0700

    Remove binding.pry

--- app/mailers/contact_me_mailer.rb
+++ app/mailers/contact_me_mailer.rb
@@ -1,7 +1,6 @@
 class ContactMeMailer < ApplicationMailer
 
 	def send_contact contact
-		binding.pry
 		mail to: 'valdez.sebastian4@gmail.com',
 				subject: contact.subject,
 				from: contact.sender_email,
