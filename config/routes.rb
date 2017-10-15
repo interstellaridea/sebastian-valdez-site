25fa35cd8286d95f19206b8cde2dab97ba371843
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-10-14 23:43:49 -0700

    update the route and btn paths for new download action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get  'resume-download', to: 'welcome#download_resume',    as: 'download_resume'
+  get  'resume-download', to: 'pages#download_resume', as: 'download_resume'
   get 	'about-me',	to: 'pages#about_me'
   post 'contact_me', to: 'pages#contact'
   root 'pages#home'
