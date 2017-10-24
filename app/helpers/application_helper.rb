42a2fb68a1dea4928799c65b918f517bb1f48403
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-10-23 20:53:22 -0700

    Add alerts for when logging in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,13 @@ module ApplicationHelper
 	def generate_copyright
 		SebViewTool::Renderer.copyright 'Sebastian Valdez', 'All rights reserved'
 	end
+
+	def flash_class(level)
+		case level
+			when 'notice' then "alert alert-info"
+			when 'success' then "alert alert-succesles"
+			when 'error' then "alert alert-danger"
+			when 'alert' then "alert alert-danger"
+		end
+	end
 end
