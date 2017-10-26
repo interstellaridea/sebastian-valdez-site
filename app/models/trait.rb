92d690efa71be77b48f2f03cb11005b194ee2fbc
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-10-26 00:59:32 -0700

    Create custom scope to return admin trait uploads

--- app/models/trait.rb
+++ app/models/trait.rb
@@ -5,4 +5,7 @@ class Trait < ApplicationRecord
   mount_uploader :resume, ResumeUploader
   mount_uploader :profile_picture, ProfilePictureUploader
 
+  # TODO, update where qury limits to one, and is also looking for state == live
+  scope :admin_posts, -> { where(user_id: (User.find_by(roles: 'admin').id)) }
+
 end
