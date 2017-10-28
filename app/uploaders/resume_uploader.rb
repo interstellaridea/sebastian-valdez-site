66fcb8c14c3b7d42a3767f42ac08b374ca4e9196
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-10-27 21:35:31 -0700

    Implement AWS s3 uploading

--- app/uploaders/resume_uploader.rb
+++ app/uploaders/resume_uploader.rb
@@ -4,7 +4,7 @@ class ResumeUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :aws
   # storage :fog
 
 
