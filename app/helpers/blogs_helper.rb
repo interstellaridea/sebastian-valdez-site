163c503f2194a14152498a076755bc0d1a6f3ed4
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-11-07 21:19:05 -0800

    Create helper for state option links

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,9 +1,16 @@
 module BlogsHelper
-	def active_blog? option, blog
-		if blog.status == option
+
+	def blog_link_generator blog
+		states = Blog.statuses.keys
+		states.map{|s| link_to "#{s}", '#', class: "#{active_blog? s, blog} btn btn-sm" }.join.html_safe
+	end
+
+	def active_blog? state, blog
+		if blog.status == state
 			"btn-success"
 		else
 			"btn-secondary"
 		end
 	end
+
 end
