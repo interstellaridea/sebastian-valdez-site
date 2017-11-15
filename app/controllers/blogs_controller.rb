2a17262c27b32f99862f32365792d69bad8c8619
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-11-14 20:06:30 -0800

    Add blog#new for logged_in admin

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,11 +1,27 @@
 class BlogsController < ApplicationController
 	before_action :set_blog, only: [ :show, :toggle_status ]
+
 	def index
 		@blogs = Blog.all
 	end
 
 	def show
 		@blog = Blog.find(params[:id])
+	end
+
+	def new
+		@blog = Blog.new
+	end
+
+	def create
+		@blog = Blog.new(blog_params)
+
+		if @blog.valid?
+			@blog.save!
+			redirect_to blogs_path, notice: 'You successfully created a blog!'
+		else
+			render :new
+		end
 	end
 
 	def toggle_status
@@ -29,5 +45,9 @@ class BlogsController < ApplicationController
 	private
 		def set_blog
 			@blog = Blog.find(params[:id])
+		end
+
+		def blog_params
+			params.require(:blog).permit(:title, :content)
 		end
 end
