1411caa03bd618200b0e93c3050ae8b0b3dda454
Sebastian Valdez <valdez.sebastian4@gmail.com>
2017-12-21 23:50:58 -0800

    Add scopes: recent & pending_editor_approval

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,8 @@ class Blog < ApplicationRecord
 	validates_presence_of :title
 
 	enum status: { draft: 0, pending: 1, approved: 2, published: 3 }
+
+  scope :pending_editor_approval, -> { where.not(status: [0,3]) } # only pending & approved blogs 
+  
+  scope :recent, -> { order(updated_at: :desc) }
 end
