e53cb4f801894fefc83eb707cff466e02465518a
Sebastian Valdez <valdez.sebastian4@gmail.com>
2018-01-05 16:07:47 -0800

    Include uploader for blog photos

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,8 @@
 class Blog < ApplicationRecord
 	validates_presence_of :title
 
+  mount_uploader :photo, BlogPhotoUploader
+
 	enum status: { draft: 0, pending: 1, approved: 2, published: 3 }
 
   scope :pending_editor_approval, -> { where.not(status: [0,3]) } # only pending & approved blogs 
